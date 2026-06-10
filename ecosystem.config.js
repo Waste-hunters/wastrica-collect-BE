843773c329075448d40ce893a04449f9d870c640
ntwali-1 <ntwaliyanis@gmail.com>
2026-06-10 09:58:46 +0200

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'wastrica-collect-be',
-      script: 'dist/main.js',
+      script: 'dist/src/main.js',
       instances: 1,
       autorestart: true,
       watch: false,
